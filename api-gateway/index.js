d842eb2da41188fd745c74e82d65e484400a7054
Ranjan <ranjan@pearlorganisation.com>
2025-04-18 11:13:49 +0530

    new changes

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,18 +1,26 @@
+// api-gateway/index.js
+
 import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import {
+  createProxyMiddleware
+} from 'http-proxy-middleware';
 
 const app = express();
 
 app.use('/api/users', createProxyMiddleware({
-  target: 'http://user-service:3001',
+  target: 'http://user-service:3001', // <-- CHANGED
   changeOrigin: true,
-  pathRewrite: { '^/api/users': '' }
+  pathRewrite: {
+    '^/api/users': ''
+  }
 }));
 
 app.use('/api/sessions', createProxyMiddleware({
-  target: 'http://session-service:3002',
+  target: 'http://session-service:3002', // <-- CHANGED
   changeOrigin: true,
-  pathRewrite: { '^/api/sessions': '' }
+  pathRewrite: {
+    '^/api/sessions': ''
+  }
 }));
 
 app.listen(3000, () => console.log('API Gateway on 3000'));
