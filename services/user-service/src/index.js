44d096d67542fcd529ea42558fe774bf3f03b822
Ranjan <ranjan@pearlorganisation.com>
2025-04-24 11:05:49 +0530

    updated docker.compose yml for

--- services/user-service/src/index.js
+++ services/user-service/src/index.js
@@ -7,9 +7,11 @@ const app = express();
 app.use(express.json());
 app.use('/', userRoutes);
 
+
+startUserSessionConsumer()
 mongoose.connect('mongodb+srv://manishcodes117:KhTNLpyVBVrnN3MC@cluster0.jhzbrfg.mongodb.net/?retryWrites=true&w=majority&appName=users')
   .then(() => {
     app.listen(3001, () => console.log('User service on 3001'))
-    startUserSessionConsumer()
+    // startUserSessionConsumer()
   })
   .catch(err => console.error('MongoDB connection failed:', err));
